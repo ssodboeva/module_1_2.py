8e17599e45d1a2267183991ee9e87f73bd917deb
ssodboeva <radnaevas87@gmail.com>
2024-10-01 00:26:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+homework_amount = 12
+time_spent_amount = 1.5
+name = 'Python'
+hours = time_spent_amount/homework_amount
+print ('Курс:', name, 'всего задач:', homework_amount, 'затрачено часов:', time_spent_amount, 'среднее время выполнения', hours)
